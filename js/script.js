ebbbd9f3edda358281958ef2a599511744293fe2
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-08-25 18:19:02 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var vu = "Website VuNghiXuan";
+ 
+// alert(vu);
+console.log(vu)
+
+// Đổi màu chữ: Chưa làm dc
+function myFunction() {
+    document.getElementById("thongtincanhan").style.fontSize = "25px"; 
+    document.getElementById("thongtincanhan").style.color = "red";
+    document.getElementById("thongtincanhan").style.backgroundColor = "yellow";        
+  }
