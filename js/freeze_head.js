b31d579012e4e6921137a202ee76e6a35fde28f4
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2022-11-13 07:23:31 +0700

    Add files via upload

--- js/freeze_head.js
+++ js/freeze_head.js
@@ -1,15 +1,3 @@
-var vu = "Website VuNghiXuan";
- 
-// alert(vu);
-console.log(vu)
-
-// Đổi màu chữ: Chưa làm dc
-function myFunction() {
-    document.getElementById("thongtincanhan").style.fontSize = "25px"; 
-    document.getElementById("thongtincanhan").style.color = "red";
-    document.getElementById("thongtincanhan").style.backgroundColor = "yellow";        
-  }
-
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
